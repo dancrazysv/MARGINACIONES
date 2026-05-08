3a56a138a7a01bd3b2169d9fa58b547624a00500
DANIEL HERNANDEZ <dhernande743@alumno.uned.es>
2026-05-08 08:31:50 -0600

    Add files via upload

---
+++ main_app/ControlCalidad/vistas/parte_superior.php
@@ -0,0 +1,60 @@
+<?php
+date_default_timezone_set('America/Costa_Rica');
+
+ ?>
+
+<link rel="stylesheet" type="text/css" href="css/sb-admin-222.css">
+
+
+   
+<body>
+<div class="page-wrapper chiller-theme toggled">
+  <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+   <h4><font size="+3" color="#FFFFFF"> <i class="fas fa-bars"></i></font></h4>
+  </a>
+  <nav id="sidebar" class="sidebar-wrapper">
+    <div class="sidebar-content">
+      <div class="sidebar-brand">
+        <a href="#">Menú</a>
+        <div id="close-sidebar">
+          <i class="far fa-times-circle"></i>
+        </div>
+      </div>
+      
+      <!-- sidebar-header  -->
+      
+      <!-- sidebar-search  -->
+      <div class="sidebar-menu">
+        <ul>
+          <li class="header-menu">
+            <span>Usuarios</span>
+          </li>
+         
+      
+          <li class="sidebar-dropdown">
+            <a href="#">
+              <i class="fas fa-file-contract"></i>
+              <span>Varios Documentos</span>
+            </a>
+            <div class="sidebar-submenu">
+              <ul>
+               
+                <li>
+                  <a href="#"><button class="btn btn-link" type="button" name="btnaper" id="btnaper" data-toggle="tooltip" data-placement="bottom" ><span class="stretched-link" data-toggle="modal" data-target="#apercierre"><i class="fas fa-file-alt" style="color: #FFFFFF; font-size: 12px">&nbsp;Aperturas y Cierres</i></spam></button></a>
+                </li>
+               
+              </ul>
+            </div>
+          </li>
+         
+ 
+        
+ 
+                
+        </ul>
+      </div>
+      <!-- sidebar-menu  -->
+    </div>
+
+  </nav>
+  
